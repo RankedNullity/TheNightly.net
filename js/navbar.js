aba57c4744c42d67e05710e738a8ba763d91cd26
Jaron <jierui1998@gmail.com>
2018-12-06 14:39:43 -0800

    Updated repository structure.

---
+++ js/navbar.js
@@ -0,0 +1,34 @@
+/**
+ * Jaron Wang
+ * Javascript for the top navigation bar.
+ */
+(function() {
+  const navBarHeight = 50;
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window. When this event occurs,
+   * the attached function (initialize) will be called.
+   */
+  window.addEventListener("load", initialize);
+
+  let mainNav;
+  function initialize() {
+    mainNav = document.getElementById('Main-Nav');
+  }
+
+  // When the user scrolls the page, execute scrollCheck
+  window.addEventListener('scroll', scrollCheck);
+
+  // Get the navbar
+
+  function scrollCheck() {
+    //console.log("y: " + window.pageYOffset);
+    if(window.pageYOffset >= navBarHeight) {
+      mainNav.classList.add("scrolled-top-nav");
+    } else {
+      mainNav.classList.remove("scrolled-top-nav");
+    }
+  }
+  // Add the navBarHeightclass to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+})();
